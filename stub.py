00874bfa9bc8ed23c2d5a0112402143291507acb
snar5 <snar5@yahoo.com>
2016-02-08 21:47:15 -0600

    updated

--- stub.py
+++ stub.py
@@ -10,11 +10,11 @@ def sniffProbe(p):
     if p.haslayer(Dot11Elt):
         if p.type ==0 and p.subtype ==8:
             channel = int(ord(p[Dot11Elt:3].info))
-            print "Info: %s %s" % (channel, p.info)
-            extra = p.notdecoded
-            rssi = binascii.hexlify(extra)
-
-            print binascii.b2a_uu(rssi)
-            print p.name 
+            
+            print "Info: %s %s" % (channel, p[Dot11Elt:].info)
+            print len(p[RadioTap].notdecoded)
+            extra = 256-int(ord(p[RadioTap].notdecoded[26]))
+            print extra
+            
 sniff(iface=interface,store=0,count=0,prn=sniffProbe)
 
