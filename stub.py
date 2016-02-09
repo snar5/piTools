9af9149e0dc824ca0cd815030d974f1098592fbe
Mark <snar5@yahoo.com>
2016-02-09 10:26:58 -0600

    Updated

--- stub.py
+++ stub.py
@@ -3,18 +3,22 @@ from scapy.all import *
 import binascii
 
 interface = 'wlan0mon'
-probeReqs = []
+probeReqs = {}
 
 def sniffProbe(p):
 
     if p.haslayer(Dot11Elt):
         if p.type ==0 and p.subtype ==8:
-            channel = int(ord(p[Dot11Elt:3].info))
-            
-            print "Info: %s %s" % (channel, p[Dot11Elt:].info)
-            print len(p[RadioTap].notdecoded)
-            extra = 256-int(ord(p[RadioTap].notdecoded[26]))
-            print extra
+            		channel = int(ord(p[Dot11Elt:3].info))
+            		name = p.info
+			power = 256 - int(ord(p[RadioTap].notdecoded[26]))
+			msg = "AP: %s Pwer: %s Channel: %s" % (name,power,channel)
+			probeReqs[p.addr2] = msg
+		
+            #print "Info: %s %s" % (channel, p[Dot11Elt:].info)
+            #print len(p[RadioTap].notdecoded)
+            #extra = 256-int(ord(p[RadioTap].notdecoded[26]))
+        print probeReqs
             
 sniff(iface=interface,store=0,count=0,prn=sniffProbe)
 
